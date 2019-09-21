7fdb4c1b67edf8a8594f4fd76e230780b3d50ede
gamefreak20 <joeystil3@gmail.com>
2019-09-21 21:39:06 +0200

    section 10 done

--- angular/section3/src/app/recipes/recipe.service.ts
+++ angular/section3/src/app/recipes/recipe.service.ts
@@ -1,19 +1,36 @@
 import {Recipe} from "./recipe.model";
-import {EventEmitter} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a test',
-      'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg'),
+      'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg',
+      [
+        new Ingredient('meat', 1),
+        new Ingredient('fries', 3),
+      ]),
     new Recipe('A Test Recipe2', 'This is a test',
-      'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg'),
+      'http://technicallyteamann.com/wp-content/uploads/2017/03/Recipe_logo-1.jpeg',
+      [
+        new Ingredient('meats', 3),
+        new Ingredient('fries', 3),
+      ]),
   ];
+
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 
 }

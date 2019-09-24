0758fa7cabcafc1c2ee4fc5e4895b36602372045
gamefreak20 <joeystil3@gmail.com>
2019-09-24 22:02:55 +0200

    section 12 done

--- angular/section3/src/app/recipes/recipe.service.ts
+++ angular/section3/src/app/recipes/recipe.service.ts
@@ -1,4 +1,4 @@
-import {Recipe} from "./recipe.model";
+import {Recipe} from './recipe.model';
 import {EventEmitter, Injectable} from "@angular/core";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
@@ -31,6 +31,10 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
   }
 
 }
